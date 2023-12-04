56cfdf80e3c55225229f1c2aa86b4abdcc5859c3
ashik <ashik.os@cied.in>
2023-12-04 15:09:40 +0530

    serialiser added

--- proj_1/proj_1/product/serializers.py
+++ proj_1/proj_1/product/serializers.py
@@ -2,8 +2,39 @@ from rest_framework import serializers
 from .models import *
 
 
+class VarirtyImagesSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        """meta info"""
+
+        model = VarirtyImages
+        fields = "__all__"
+
+class ProductVarietySerializer(serializers.ModelSerializer):
+
+    varity_images = VarirtyImagesSerializer(many=True, source='images')
+    
+    class Meta:
+        """meta info"""
+
+        model = ProductVariety
+        fields = "__all__"
+
+
+class ProductSerilaiser(serializers.ModelSerializer):
+    
+    class Meta:
+        """meta info"""
+
+        model = Product
+        fields = "__all__"
+
+    varity = ProductVarietySerializer(many=True,source='varieties')
+
+
 
 class BaseProductSerializer(serializers.ModelSerializer):
+
 
     class Meta:
         """meta info"""
@@ -11,55 +42,6 @@ class BaseProductSerializer(serializers.ModelSerializer):
         model = BaseProduct
         fields = "__all__"
 
-    products = serializers.SerializerMethodField(required=False)
-
-    def get_products(self, instance):
-
-        """function to get details of a product"""
-
-        products = instance.products.all()
-
-        product_list = []
-
-        for item in products:
-            data = {
-                "id": item.id,
-                "item_hsn": item.item_hsn,
-                "item_code": "100", 
-                "mrp": "85.00",
-                "varity": []
-                }
-            varieties = item.varieties.all()
-            for variety in varieties:
-                prod_variety = {
-                    "variation_name": variety.variation_name,
-                    "variation_value": variety.variation_value,
-                    "created_at": variety.created_at,
-                    "updated_at": variety.updated_at,
-                    "varity": variety.variety,
-                    "varity_images": []
-                }
-                images = variety.images.all()
-                for image in images:
-                    image_data = {
-                        "id": image.id,
-                        "variantproduct_image": image.image.url,
-                        "created_at":image.created_at,
-                        "updated_at": image.updated_at,
-                        "varity_images": 24
-                    }
-
-                    prod_variety['varity_images'].append(image_data)
-                data["varity"].append(prod_variety)
-
-            product_list.append(data)
-
-        return product_list
-
-
-
-
-
-
+    product = ProductSerilaiser(many=True, source='products')
 
 
